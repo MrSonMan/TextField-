c872fcd5e39515a3c5c82ce6f798acb1e37b9190
kweonyoungmin <kweonyoungmin@kweonyoungminui-iMac.local>
2015-05-02 19:17:52 +0900

    Lesson3장 최종

--- TextFields/MoneyTextFieldDelegate.swift
+++ TextFields/MoneyTextFieldDelegate.swift
@@ -10,45 +10,30 @@ import Foundation
 import UIKit
 
 class MoneyTextFieldDelegate: NSObject, UITextFieldDelegate {
-    
-    func textFieldDidBeginEditing(textField: UITextField) {
-        textField.text = ""
-        //var i = 0
-    }
-    
-    var i = 0
-    var text = ""
-    var text2 = ""
-    var text3 = ""
-    var lastText = ""
+  
+    var oldNum = 0
+    var newNum = 0
+    var resultNum = 0.0
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         
-        i = i+1
-        
         if string != ""{
-            if i == 1{
-                text = string
-                textField.text = "$0.0" + text
-                return false
-            }else if i == 2{
-                text2 = string
-                textField.text = "$0." + text + text2
-                return false
-            }else if i == 3{
-                text3 = string
-                textField.text = "$" + text + "." + text2 + text3
-                lastText = text + "." + text2 + text3
-                return false
-            }else{
-                lastText = lastText + string
-                textField.text = "$" + lastText
-                return false
-            }
-        }else if string == ""{
-            textField.text = ""
-            i = 0
+            newNum = string.toInt()!
+            
+            oldNum = oldNum * 10 + newNum
+            
+            resultNum = Double(oldNum) / 100
+            
+            textField.text = "$\(resultNum)"
+            return false
+        }else{
+            oldNum = oldNum / 10
+            resultNum = Double(oldNum) / 100
+            textField.text = "$\(resultNum)"
+            println("newNum : \(newNum) // oldNum : \(oldNum)")
+            return false
         }
-        return true
+        
     }
+    
 }
